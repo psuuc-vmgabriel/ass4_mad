42369d5ff55c57de5ee83a36a29cb08fd971ca8d
Vina Marie <132547318+psuuc-vmgabriel@users.noreply.github.com>
2025-03-03 13:08:47 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -25,22 +25,98 @@ class _MainScreenState extends State<MainScreen> {
   final List<Map<String, String>> _song = [
     {'title': 'Home - Daughtry', 'path': 'assets/audio/song1.mp3'},
     {'title': 'Far Away - Nikelback', 'path': 'assets/audio/song2.mp3'},
-    {'title': 'Whereever You Will Go - The Calling ', 'path': 'assets/audio/song3.mp3'},
+    {
+      'title': 'Wherever You Will Go - The Calling ',
+      'path': 'assets/audio/song3.mp3',
+    },
     {'title': 'Wonderwall - Aosis', 'path': 'assets/audio/song4.mp3'},
     {'title': 'Collide - Howie Day', 'path': 'assets/audio/song5.mp3'},
   ];
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  int? _currentIndex;
+  void _openMusicPlayer(BuildContext context, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (context) => MusicPlayerScreen(songTitle: _song[index]['title']!,
+            songPath: _song[index]['path']!,),
+      ),
+    );
+  }
 
-  void _playSong(int index) async {
-    if (_currentIndex == index) {
-      _audioPlayer.stop();
-      setState(() => _currentIndex = null);
+  // final AudioPlayer _audioPlayer = AudioPlayer();
+  // int? _currentIndex;
+
+  // void _playSong(int index) async {
+  //   if (_currentIndex == index) {
+  //     _audioPlayer.stop();
+  //     setState(() => _currentIndex = null);
+  //   } else {
+  //     await _audioPlayer.setAsset(_song[index]['path']!);
+  //     _audioPlayer.play();
+  //     setState(() => _currentIndex = index);
+  //   }
+  // }
+
+  @override
+  // void dispose() {
+  //   _audioPlayer.dispose();
+  //   super.dispose();
+  // }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Favorite Songs')),
+      body: ListView.builder(
+        itemCount: _song.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.music_note, color: Colors.pink),
+            trailing: Icon(Icons.play_arrow, color: Colors.red),
+            title: Text(_song[index]['title']!),
+            onTap: () => _openMusicPlayer(context, index),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MusicPlayerScreen extends StatefulWidget {
+  final String songTitle;
+  final String songPath;
+
+  const MusicPlayerScreen({
+    super.key,
+    required this.songTitle,
+    required this.songPath,
+  });
+
+  @override
+  State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSong();
+  }
+
+  Future<void> _loadSong() async {
+    await _audioPlayer.setAsset(widget.songPath);
+  }
+
+  void _togglePlayPause() {
+    if (_isPlaying) {
+      _audioPlayer.pause();
     } else {
-      await _audioPlayer.setAsset(_song[index]['path']!);
       _audioPlayer.play();
-      setState(() => _currentIndex = index);
     }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
   }
 
   @override
@@ -51,17 +127,26 @@ class _MainScreenState extends State<MainScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Favorite Songs')),
-      body: ListView.builder(
-        itemCount: _song.length,
-        itemBuilder: (context, index){
-          bool isPlaying = _currentIndex == index;
-          return ListTile(
-            leading: Icon(isPlaying? Icons.pause_circle: Icons.play_circle),
-            title: Text(_song[index]['title']!),
-            onTap: ()=> _playSong(index),
-          );
-        },
+      appBar: AppBar(title: Text(widget.songTitle)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              _isPlaying ? Icons.music_note : Icons.music_off,
+              size: 100,
+              color: Colors.pinkAccent,
+            ),
+            SizedBox(height: 20),
+            Text(
+              widget.songTitle,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+          ElevatedButton.icon(onPressed: _togglePlayPause, icon: Icon(_isPlaying? Icons.pause : Icons.play_arrow), label: Text(_isPlaying? 'Pause': 'Paly'),
+          style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15)),)
+          ],
+        ),
       ),
     );
   }
